a6a262f4f93f4e213bf375dfaac9b9f9b8007f42
hh <hh4142019@outlook.com>
2022-05-05 21:25:29 +0800

    GUI Day 2

--- PySimpleGUI/form_finding_psg.py
+++ PySimpleGUI/form_finding_psg.py
@@ -11,13 +11,13 @@ sg.theme('DarkAmber')
 
 # All the stuff inside your window.
 layout = [[sg.Text('1, 选择模式/Select a Mode')],
-          [sg.Radio('单索/Cable or Arch', "RadioDemo", default=True, size=(20, 1)),
+          [sg.Radio('单索/Cable or Arch', "RadioDemo", default=True, size=(20, 1), key = "001"),
            sg.Radio('索网/Web', "RadioDemo", size=(10, 1)),
            sg.Radio('SAP2000', "RadioDemo", size=(10, 1))],
           [sg.HSep()],
-          [sg.Text('2, 设置参数/Parameters')],
+          [sg.Text('2, 设置约束/Constrains')],
           [sg.HSep()],
-          [sg.Text('3, 数据检查/Data Check')],
+          [sg.Text('3, 设置预荷载/Pre-loading')],
           [sg.HSep()],
           [sg.Text('4,'), sg.Button("润/Run")],
           [sg.Multiline(size=(70, 5))],
@@ -25,13 +25,17 @@ layout = [[sg.Text('1, 选择模式/Select a Mode')],
           [sg.Text('Enter something on Row 2'), sg.InputText()],
           [sg.Button('Ok'), sg.Button('Cancel')]]
 
+
 # Create the Window
 window = sg.Window('结构找形/Form-finding by HH', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
+    # print('You entered ', values)
     if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    
 
 window.close()
